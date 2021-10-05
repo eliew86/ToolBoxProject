4d2c2ea9ef357881bd1f5e614289b40de6f961c8
Elie Wanna <elie.w@hotmail.ca>
2021-10-05 15:56:01 -0400

    comm system almost done

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -3,76 +3,56 @@ import TextField from "@material-ui/core/TextField"
 import styled from "styled-components";
 import io from "socket.io-client";
 import Header from "./Header";
-
-
 import "./Chat.css"
+import ChatComm from "./ChatComm";
+
+const socket = io.connect("http://localhost:4000")
+
 
 // const socket = io.connect("http://localhost:4000")
 const Chat = () => {
-	const [ state, setState ] = useState({ message: "", name: "" })
-	const [ chat, setChat ] = useState([])
+	const [username, setUsername] = useState("")
+    const [room, setRoom] = useState("")
+    const [showChat, setShowChat] = useState(false);
+	
+    const joinRoom = () => {
 
-	const socketRef = useRef()
-
-	useEffect(
-		() => {
-			socketRef.current = io.connect("http://localhost:4000");
-			socketRef.current.on("message", ({ name, message }) => {
-				setChat([ ...chat, { name, message } ])
-			})
-			return () => socketRef.current.disconnect()
-		},
-		[ chat ]
-	)
-
-	const onTextChange = (e) => {
-		setState({ ...state, [e.target.name]: e.target.value })
-	}
-
-	const onMessageSubmit = (e) => {
-		const { name, message } = state
-		socketRef.current.emit("message", { name, message })
-		e.preventDefault()
-		setState({ message: "", name })
-	}
-
-	const renderChat = () => {
-		return chat.map(({ name, message }, index) => (
-			<div key={index}>
-				<h3>
-					{name}: <span>{message}</span>
-				</h3>
-			</div>
-		))
-	}
+        if(username !== "" && room !== "") {
+            socket.emit("join_room", room)
+            setShowChat(true);
+        }
+    }
 
 	return (
         <>
             <Header />
             <Wrapper>
-                <div className="card">
-                    <form onSubmit={onMessageSubmit}>
-                        <h1>Messenger</h1>
-                        <div className="name-field">
-                            <TextField name="name" onChange={(e) => onTextChange(e)} value={state.name} label="Name" />
-                        </div>
-                        <div>
-                            <TextField
-                                name="message"
-                                onChange={(e) => onTextChange(e)}
-                                value={state.message}
-                                id="outlined-multiline-static"
-                                variant="outlined"
-                                label="Message"
-                            />
-                        </div>
-                        <button>Send Message</button>
-                    </form>
-                    <div className="render-chat">
-                        <h1>Chat Log</h1>
-                        {renderChat()}
+                <div className="App">
+                    {!showChat ? (
+                    <div className="joinChatContainer">
+                        <h3>Join a Chat</h3>
+                        <input 
+                            type="text" 
+                            placeholder="John..."
+                            onChange={(event) => {setUsername(event.target.value)}}
+                        />
+                        <input 
+                            type="text" 
+                            placeholder="Room ID..."
+                            onChange={(event) => {setRoom(event.target.value)}}
+                        />
+                        <button onClick={joinRoom}>Join a Room</button>
                     </div>
-                </div>
+                    )
+                    : (
+                        <ChatComm 
+                            socket={socket}
+                            username={username}
+                            room={room}
+                        />
+                    )}
+                    </div>
+
             </Wrapper>
         </>
 	)
